482974c0d35a2169120c286fe2b3c871f0a23464
hunterchen <hunterchen@HunterdeiMac.local>
2017-02-15 17:05:38 +0800

    add "Swipe Delete Row" function

--- TableView_byswift3/TableView_byswift3/TableViewController.swift
+++ TableView_byswift3/TableView_byswift3/TableViewController.swift
@@ -53,6 +53,7 @@ class TableViewController: UITableViewController {
         
     }
     func refreshAction(){
+        
         i_rows = i_rows + 10
         self.tableView.reloadData()
     }
@@ -146,17 +147,18 @@ class TableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            dataArray.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
